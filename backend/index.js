9ddc759cebd522a5acb5e11756371854d5e44a46
Ujjwal Bhatt <ujjwalbhatt09@gmail.com>
2024-02-07 12:41:24 +0530

    did some minor changes in the backend code

--- backend/index.js
+++ backend/index.js
@@ -1,75 +1,63 @@
 //basic express boilerplate code
 const express = require("express");
 const { createTodo, updateTodo } = require("./types");
-const {Todo} = require("./database");
+const { Todo } = require("./database");
 const app = express();
 const port = 3000;
 
 //middleware to parse json
 app.use(express.json());
 
+app.post("/createTodo", async (request, res) => {
+  const todoBody = request.body;
+  const response = createTodo.safeParse(todoBody);
 
-app.post("/createTodo", async (request, res)=>{
-     const todoBody = request.body;
-     const response = createTodo.safeParse(todoBody);
+  if (!response.success) {
+    return res.status(411).json({
+      message: "Invalid input",
+    });
+  }
 
-     if(!response.success){
-        return res.status(411).json({
-            message: "Invalid input"
-        })
-     }
+  //put todo in database
+  //  const todo = new Todo({
+  //     title: todoBody.title,
+  //     description: todoBody.description,
+  //     completed: todoBody.completed
+  //  })
 
-     //put todo in database
-    //  const todo = new Todo({
-    //     title: todoBody.title,
-    //     description: todoBody.description,
-    //     completed: todoBody.completed
-    //  })
+  const savedTodo = await Todo.create({
+    title: todoBody.title,
+    description: todoBody.description,
+    completed: false,
+  });
 
-     const savedTodo = await Todo.create({
-        title: todoBody.title,
-        description: todoBody.description,
-        completed: false
-     });
+  return res.json(savedTodo);
+});
 
-     return res.json(savedTodo);
+app.get("/getTodos", async (requesst, response) => {
+  return response.json(await Todo.find());
+});
 
-})
+app.put("/markAsDone", async (request, res) => {
+  const idObject = request.body;
+  const response = updateTodo.safeParse(idObject);
 
+  if (!response.success) {
+    return res.status(411).json({
+      message: "Invalid inputs",
+    });
+  }
 
-app.get("/getTodos", async (requesst, response)=>{
+  await Todo.updateOne(
+    {
+      _id: request.body.id,
+    },
+    {
+       completed: true
+    }
+  );
 
-    return response.json(await Todo.find());
-})
-
-
-app.put("/markAsDone", async (request, res)=>{
-       const idObject = request.body;
-       const response = updateTodo.safeParse(idObject);
-
-       if(!response.success){
-        return res.status(411).json({
-            message: "Invalid inputs"
-        })
-       }
-
-       const _id = idObject.id;
-
-       await Todo.updateOne({
-        _id
-       }, {
-        "$set":{
-            completed: true
-        }
-       })
-
-       return res.status(200).json({message: "Todo updated"});
-})
-
-
-
-
-
-
+  return res.status(200).json({ message: "Todo updated" });
+});
 
 app.listen(port);
